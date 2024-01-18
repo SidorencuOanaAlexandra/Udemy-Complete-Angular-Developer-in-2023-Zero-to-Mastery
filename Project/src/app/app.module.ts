4e36d851d2223114244aef6afdf1c332ecde2270
Sidorencu Oana <mihai.fuia.work@gmail.com>
2024-01-18 13:44:04 +0200

    removed post component

--- Project/src/app/app.module.ts
+++ Project/src/app/app.module.ts
@@ -3,14 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { PostComponent } from './components/post/post.component';
 import { UserModule } from './user/user.module';
 import { NavComponent } from './nav/nav.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PostComponent,
     NavComponent
   ],
   imports: [
