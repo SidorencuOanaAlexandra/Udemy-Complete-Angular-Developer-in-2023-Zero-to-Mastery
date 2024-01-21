0da18275e56ef0098e26ef59bb061ef0e75d4224
Sidorencu Oana <mihai.fuia.work@gmail.com>
2024-01-21 15:44:11 +0200

    used content children decorator to get tabs

--- Project/src/app/shared/tabs-container/tabs-container.component.ts
+++ Project/src/app/shared/tabs-container/tabs-container.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, AfterContentInit, OnInit, ContentChild } from '@angular/core';
+import { TabComponent } from '../tab/tab.component';
 
 @Component({
   selector: 'app-tabs-container',
   templateUrl: './tabs-container.component.html',
   styleUrls: ['./tabs-container.component.css']
 })
-export class TabsContainerComponent implements OnInit {
+export class TabsContainerComponent implements AfterContentInit {
+  @ContentChild(TabComponent) tabs = {}
 
   constructor() { }
 
-  ngOnInit(): void {
+  ngAfterContentInit(): void {
+    console.log(this.tabs);
   }
 
 }
