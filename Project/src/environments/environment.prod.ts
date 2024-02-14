552c84f062832cfcc32614bc384e8f6626ce2b5c
SidorencuOanaAlexandra <osidorencu@yahoo.com>
2024-02-14 15:24:28 +0200

    integrated firebase in the project

--- Project/src/environments/environment.prod.ts
+++ Project/src/environments/environment.prod.ts
@@ -1,3 +1,10 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyDR5TgL6kg_q_zmJCwAYQPTAS-Obdq9rXg",
+    authDomain: "clips-6f918.firebaseapp.com",
+    projectId: "clips-6f918",
+    storageBucket: "clips-6f918.appspot.com",
+    appId: "1:978907562101:web:446da459067b856fcbb83c"
+  }
 };
--- Project/src/environments/environment.ts
+++ Project/src/environments/environment.ts
@@ -3,7 +3,14 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyDR5TgL6kg_q_zmJCwAYQPTAS-Obdq9rXg",
+    authDomain: "clips-6f918.firebaseapp.com",
+    projectId: "clips-6f918",
+    storageBucket: "clips-6f918.appspot.com",
+    appId: "1:978907562101:web:446da459067b856fcbb83c"
+  }
 };
 
 /*
