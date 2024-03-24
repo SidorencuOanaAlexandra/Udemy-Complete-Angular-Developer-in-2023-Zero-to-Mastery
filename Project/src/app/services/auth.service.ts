5530f250465cd63929fa25274170cf2b469868eb
SidorencuOanaAlexandra <osidorencu@yahoo.com>
2024-03-24 14:48:14 +0200

    received current route date and redirected the user after log out if the user does not have permission on current page

--- Project/src/app/services/auth.service.ts
+++ Project/src/app/services/auth.service.ts
@@ -2,8 +2,8 @@ import { Injectable } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
 import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/compat/firestore';
 import IUser from '../models/user.model';
-import { Observable, delay, map } from 'rxjs';
-import { Router } from '@angular/router';
+import { Observable, delay, map, filter, switchMap, of } from 'rxjs';
+import { ActivatedRoute, Router, NavigationEnd } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
@@ -12,8 +12,12 @@ export class AuthService {
   private userCollection: AngularFirestoreCollection<IUser>
   public isAuthenticated$: Observable<boolean>
   public isAuthenticatedWithDelay$: Observable<boolean>
+  public redirect = false
 
-  constructor(private auth: AngularFireAuth, private db: AngularFirestore, private router: Router) {
+  constructor(private auth: AngularFireAuth,
+    private db: AngularFirestore,
+    private router: Router,
+    private route: ActivatedRoute) {
     this.userCollection = db.collection('users')
     this.isAuthenticated$ = auth.user.pipe(
       map(user => !!user)
@@ -21,6 +25,13 @@ export class AuthService {
     this.isAuthenticatedWithDelay$ = this.isAuthenticated$.pipe(
       delay(1000)
     )
+    this.router.events.pipe(
+      filter(e => e instanceof NavigationEnd),
+      map(e => this.route.firstChild),
+      switchMap(route => route?.data ?? of({ authOnly: false }))
+    ).subscribe((data) => {
+      this.redirect = data.authOnly ?? false
+    })
   }
 
   public async createUser(userData: IUser) {
@@ -59,6 +70,9 @@ export class AuthService {
     }
 
     await this.auth.signOut()
-    await this.router.navigateByUrl('/')
+
+    if (this.redirect == true) {
+      await this.router.navigateByUrl('/')
+    }
   }
 }
--- Project/src/app/video/manage/manage.component.ts
+++ Project/src/app/video/manage/manage.component.ts
@@ -11,5 +11,4 @@ export class ManageComponent implements OnInit {
 
   ngOnInit(): void {
   }
-
 }
