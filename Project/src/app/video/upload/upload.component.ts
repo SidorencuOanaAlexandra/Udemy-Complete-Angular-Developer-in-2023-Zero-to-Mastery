f59905f7005cd2c2de9400158f4394f7cc3ae6b4
SidorencuOanaAlexandra <osidorencu@yahoo.com>
2024-05-03 13:54:45 +0300

    added an alert message if the video format is incorect

--- Project/src/app/video/upload/upload.component.ts
+++ Project/src/app/video/upload/upload.component.ts
@@ -9,6 +9,9 @@ export class UploadComponent implements OnInit {
   isDragover = false
   file: File | null = null
   formIsVisible = false
+  showAlert = false
+  alertColor = 'red'
+  alertMsg = 'You must upload a mp4 video!'
 
   constructor() { }
 
@@ -21,10 +24,11 @@ export class UploadComponent implements OnInit {
     this.file = ($event as DragEvent).dataTransfer?.files.item(0) ?? null
 
     if (!this.file || this.file.type !== 'video/mp4') {
-      //this.formIsVisible = true;
+      this.showAlert = true;
     }
     else {
       this.formIsVisible = true;
+      this.showAlert = false;
     }
 
     console.log(this.file)
