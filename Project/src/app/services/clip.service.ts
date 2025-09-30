2440ea75196c76ed5bc8f482ebc6ec53c651a1e7
SidorencuOanaAlexandra <osidorencu@yahoo.com>
2025-09-30 21:53:13 +0300

    get clips

--- Project/src/app/services/clip.service.ts
+++ Project/src/app/services/clip.service.ts
@@ -10,6 +10,8 @@ import { AngularFireStorage } from '@angular/fire/compat/storage';
 })
 export class ClipService {
   public clipsCollection: AngularFirestoreCollection<IClip>
+  pageClips: IClip[] = []
+  pendingReq = false
 
   constructor(
     private db: AngularFirestore,
@@ -50,5 +52,36 @@ export class ClipService {
 
     await clipRef.delete()
     await this.clipsCollection.doc(clip.docId).delete()
+  }
+
+  async getClips() {
+    if(this.pendingReq) {
+      return
+    }
+
+    this.pendingReq = true
+    let query = this.clipsCollection.ref.orderBy('timestamp', 'desc').limit(6)
+
+    const { length } = this.pageClips
+
+    if (length) {
+      const lastDocID = this.pageClips[length - 1].docId
+      const lastDoc = await this.clipsCollection.doc(lastDocID)
+        .get()
+        .toPromise()
+
+      query = query.startAfter(lastDoc)
+    }
+
+    const snapshot = await query.get()
+
+    snapshot.forEach(doc => {
+      this.pageClips.push({
+        docId: doc.id,
+        ...doc.data()
+      })
+    })
+
+    this.pendingReq = false
   }
 }
