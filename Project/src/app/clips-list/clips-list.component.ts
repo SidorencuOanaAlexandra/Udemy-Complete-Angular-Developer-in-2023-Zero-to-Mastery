24aa2cdadc1dbfb01b3970c55ab07e1fc9e62983
SidorencuOanaAlexandra <osidorencu@yahoo.com>
2025-09-30 21:40:53 +0300

    handle scroll events on the home page to render clips when the user is at the bottom of the page

--- Project/src/app/clips-list/clips-list.component.ts
+++ Project/src/app/clips-list/clips-list.component.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ClipService } from '../services/clip.service';
 
 @Component({
   selector: 'app-clips-list',
   templateUrl: './clips-list.component.html',
   styleUrls: ['./clips-list.component.css']
 })
-export class ClipsListComponent implements OnInit {
+export class ClipsListComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  constructor(private clipService: ClipService) { }
 
   ngOnInit(): void {
+    window.addEventListener('scroll', this.handleScroll)
+  }
+
+  handleScroll = () => {
+    const { scrollTop, offsetHeight}  = document.documentElement
+    const { innerHeight } = window
+
+    const bottomOfWindow = Math.round(scrollTop) + innerHeight === offsetHeight
+
+    if(bottomOfWindow) {
+      console.log("request data")
+    }
+  }
+
+  ngOnDestroy(): void {
+    window.removeEventListener('scroll', this.handleScroll)    
   }
 
 }
